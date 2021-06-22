a26752cd775c4752d213dcbd695a2f947b8e7f25
pernst5 <85967853+pernst5@users.noreply.github.com>
2021-06-21 20:08:40 -0700

    Update ControlledDriving.ino

--- ControlledDriving.ino
+++ ControlledDriving.ino
@@ -11,17 +11,17 @@ float GyroX, GyroY, GyroZ;
 float yaw;
 float elapsedTimeMPU, currentTimeMPU, previousTimeMPU;
 float GyroErrorZ;
-int c = 0;
+int c = 0; // A counter variable for gyroError
 
 ////////////////////    For PID Control for driving straight    ////////////////////////
-double Kp = 2; // proportional constant
-double Ki = 0; // integral constant
-double Kd = 0; // derivative constant
+double P = 2; // proportional constant
+double I = 0; // integral constant
+double D = 0; // derivative constant
 
 ////////////////////    For PID Control for turning    ////////////////////////
-double Kp_t = 0.1; // proportional constant
-double Ki_t = 0; // integral constant
-double Kd_t = 5; // derivative constant
+double Kp = 0.1; // proportional constant
+double Ki = 0; // integral constant
+double Kd = 5; // derivative constant
 
 unsigned long currentTime, previousTime;
 double elapsedTime;
@@ -31,8 +31,8 @@ double input, output, setPoint;
 double cumError, rateError;
 
 ///////  For Motors  //////////
-int enA = 5; int in1 = 4; int in2 = 3; // Motor A (LEFT) pins
-int enB = 6; int in3 = 8; int in4 = 7; // Motor B (RIGHT) pins
+int enA = 5; int in1 = 4; int in2 = 3; // LEFT motor pins
+int enB = 6; int in3 = 8; int in4 = 7; // RIGHT motor pins
 int speed = 120;
 int time;
 
@@ -40,7 +40,6 @@ int time;
 int IRObstaclePin = 2;
 int buttonPin = 12;
 int motionPin = 11;
-
 
 
 void setup() {
@@ -53,11 +52,11 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  pinMode(buttonPin, INPUT);     //Button Pin will be an input
+  pinMode(buttonPin, INPUT);     // Button Pin will be an input
   pinMode(IRObstaclePin, INPUT); // Obstacle detection Pin will be an input
-  pinMode(motionPin,INPUT);
+  pinMode(motionPin,INPUT);      // Obstacle detection Pin will be an input
 
-  Serial.begin(19200);
+  Serial.begin(19200);               // Set up serial communication
   Wire.begin();                      // Initialize comunication
   Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
   Wire.write(0x6B);                  // Talk to the register 6B
@@ -66,57 +65,12 @@ void setup() {
 }
 
 void loop() {
-  // motionDetect();
-  //  delay(1000);
-  //  FindGyroError();
+
+float driveTime = 1; // Adjust the drive time in seconds
+float dtime = 700;   // Use dtime as the delay time
+
    ButtonWait();
    delay(2000);
-/*
-  while (Serial.available() == 0) {
-    // Wait for User to Input Data
-  }
-  char driveCommand = Serial.read();
-
-switch(driveCommand) {
-
-   case 'w':
-      Serial.println("Drive Forward");
-      DriveForward(speed);
-      break;
-   case 's':
-      Serial.println("Drive backwards");
-      DriveBackward(speed);
-      break;
-   case 'a':
-      Serial.println("Turn Left");
-      TurnLeft(speed);
-      break;
-   case 'd':
-      Serial.println("Turn Right");
-      TurnRight(speed);
-      break;
-   case 'x':
-      Serial.println("Stop");
-      Stop();
-      break;
-      
-   default : 
-   Serial.println("Try Again");
-}
-
-*/
-  //DriveForwardUntilObstacle(speed,1);
-  //delay(5000);
-float driveTime = 1; // In seconds
-int dtime = 200;
-  /*
-  for (int k=0;k<5;k++) {
-  }
-TurnWithGyro(90,speed);   delay(500);
-TurnWithGyro(-90,speed);   delay(500);
-TurnWithGyro(90,speed);   delay(500);
-TurnWithGyro(-90,speed);   delay(500);
-  */
 
 /// Square /////
 
@@ -138,8 +92,9 @@ TurnWithGyro(-90,speed);   delay(500);
   DriveForward(speed,driveTime);  Stop();    delay(dtime);
   TurnWithGyro(-90,speed);   delay(dtime*30);
 
-////////////////////////////////////////////////////////////
-/*
+
+////// Straight Driving Demonstration  /////
+
   DriveForward(speed,driveTime*2);  Stop();    delay(dtime*2);
   TurnWithGyro(90,speed);   delay(dtime);
   DriveForward(speed,0.3);  Stop();    delay(dtime);
@@ -159,16 +114,18 @@ TurnWithGyro(-90,speed);   delay(500);
   TurnWithGyro(-90,speed);   delay(dtime);
   DriveForward(speed,0.3);  Stop();    delay(dtime);
   TurnWithGyro(-90,speed);   delay(dtime);
-  */
-/*
-  TurnWithGyro(45,speed);   delay(dtime);
-  DriveForward(speed,driveTime*sqrt(2));  Stop();    delay(dtime);
-  TurnWithGyro(45,speed);   delay(dtime);   TurnWithGyro(90,speed); delay(dtime);
-  DriveForward(speed,driveTime);  Stop();    delay(dtime); 
+
+
+/////////////////// Angle Turning Demo /////////////////////////////
+
   TurnWithGyro(90,speed);   delay(dtime);
-  DriveForward(speed,driveTime);  Stop();    delay(dtime);
+  TurnWithGyro(-90,speed);   delay(dtime);
+  TurnWithGyro(-90,speed);   delay(dtime);
   TurnWithGyro(90,speed);   delay(dtime);
-  */
+  TurnWithGyro(175,speed);   delay(dtime);
+  TurnWithGyro(-175,speed);   delay(dtime);
+  TurnWithGyro(330,speed);   delay(dtime);
+
 }
 
 void DriveForwardUntilObstacle(int speed,float driveTime){
@@ -194,54 +151,49 @@ void DriveForward(int speed,float driveTime){
     // Set the right motor to go Forward
     digitalWrite(in4,0); digitalWrite(in3,1);
     int millis1 = millis();
-    float runTime = 0;
+    float runTime = 0; // Start runTime is 0s
 
-    while ( runTime < (driveTime * 1000) ) {                             ///// Change here
-
-      double yaw = ReadGyro();
-      output = computePID(yaw,goalAngle);
-
-      if (output > 0){
-        // Turn left
-        speedL = (speed);
-        speedR = (abs(output) * 5 + speed); // Have the speed of the right wheel to increase to turn the robot left
-         //    Serial.print(speedL); Serial.print(" / "); Serial.println(speedR);  // Print out the speed of each motor for debugging 
-      }
-      else if (output < 0){
-        // Turn Right
-        speedL = (abs(output) * 5 + speed); // Have the speed of the left wheel to increase to turn the robot left
-        speedR = (speed);
-          //   Serial.print(speedL); Serial.print(" / "); Serial.println(speedR);  // Print out the speed of each motor for debugging 
-      } else { Stop();}
-     analogWrite(enA,speedL); // Set left motor to its adjusted speed 
-     analogWrite(enB,speedR); // Set right motor to its adjusted speed 
-     //Serial.println(i);
-     runTime = (millis() - millis1);
-     Serial.println(runTime);
+    while ( runTime < (driveTime * 1000) ) {
+        double yaw = ReadGyro();
+        output = computePID(yaw,goalAngle,P,I,D);
+  
+        if (output > 0){
+          // Turn left
+          speedL = (speed);
+          speedR = (abs(output) * 5 + speed); // Have the speed of the right wheel to increase to turn the robot left
+          Serial.print(speedL); Serial.print(" / "); Serial.println(speedR);  // Print out the speed of each motor for debugging 
+        }
+        else if (output < 0){
+          // Turn Right
+          speedL = (abs(output) * 5 + speed); // Have the speed of the left wheel to increase to turn the robot left
+          speedR = (speed);
+          Serial.print(speedL); Serial.print(" / "); Serial.println(speedR);  // Print out the speed of each motor for debugging 
+        } else { Stop();}
+       analogWrite(enA,speedL); // Set left motor to its adjusted speed 
+       analogWrite(enB,speedR); // Set right motor to its adjusted speed 
+       runTime = (millis() - millis1);
     }
 }
 
 void TurnWithGyro(int goalAngle,int speed) {
   yaw = 0; // reset the heading everytime the turn function is called
-  output =  computePIDturn(yaw,goalAngle); // based on the current yaw and the goal angle compute the current output
+  output =  computePID(yaw,goalAngle,Kp,Ki,Kd); // based on the current yaw and the goal angle compute the current output
   
-  while (abs(output) > .002){
-    // delay(200);
+  while (abs(output) > .002){   // This is used to stop the robot when it is very close to the desired angle
     double yaw = ReadGyro(); // Read the current yaw value
-    //Serial.println(yaw);
-    output =  computePIDturn(yaw,goalAngle) - 0.002*goalAngle; // based on the current yaw and the goal angle compute the current output
+    output =  computePID(yaw,goalAngle,Kp,Ki,Kd); // based on the current yaw and the goal angle compute the current output
 
     if (output > 0){ 
       int speedturn = (abs(output) + speed);
       TurnLeft(speedturn);
-      // Serial.println("TurnLeft");
-      //Serial.println(speedturn);
+      Serial.println("TurnLeft");
+      Serial.println(speedturn);
     }
     if (output < 0){
       int speedturn = (abs(output) + speed);
       TurnRight(speedturn);
-      // Serial.println("TurnRight");
-      //Serial.println(speedturn);
+      Serial.println("TurnRight");
+      Serial.println(speedturn);
     }
   }
   Stop(); // Stop the robot after it has reached the correct angle
@@ -255,7 +207,7 @@ void TurnRight(int speed) {
   // Turn on Motor R Backward
   digitalWrite(in4, 1); digitalWrite(in3, 0);
   analogWrite(enB, speed); // Set left motor to input speed
-  //Serial.println("Forward --- Backward");
+  Serial.println("Forward --- Backward");
 }
 
 void TurnLeft(int speed) {
@@ -266,7 +218,7 @@ void TurnLeft(int speed) {
   // Turn on Motor R Forward
   digitalWrite(in4, 0); digitalWrite(in3, 1);
   analogWrite(enB, speed); // Set left motor to input speed
-  //Serial.println("Backward --- Forward");
+  Serial.println("Backward --- Forward");
 }
 void DriveBackward(int speed) {
   // Turn on Motor L Backward
@@ -276,14 +228,14 @@ void DriveBackward(int speed) {
   // Turn on Motor R Backward
   digitalWrite(in4, 1); digitalWrite(in3, 0);
   analogWrite(enB, speed); // Set left motor to input speed
-  //Serial.println("Backward --- Forward");
+  Serial.println("Backward --- Forward");
 }
 
 void Stop() {
   // Stop both motors by setting the speed to 0
   analogWrite(enA, 0); 
   analogWrite(enB, 0); 
-  //Serial.println("Stop");
+  Serial.println("Stop");
 }
 
 void ButtonWait() {
@@ -312,67 +264,45 @@ double ReadGyro(){
   currentTimeMPU = millis();            // Current time actual time read
   elapsedTimeMPU = (currentTimeMPU - previousTimeMPU) / 1000; // Divide by 1000 to get seconds
   Wire.beginTransmission(MPU);
-  Wire.write(0x43); // Gyro data first register address 0x43
+  Wire.write(0x47); // Gyro data first register address for the GyroZ data (0x47)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true);    // Read 4 registers total, each axis value is stored in 2 registers
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+  Wire.requestFrom(MPU, 2, true);    // Read 2 registers total
+  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
   // Correct the outputs with the calculated error values
-  GyroZ = GyroZ + 0.85; // GyroErrorZ ~ (-0.88)
+  GyroZ = GyroZ + 0.85; // GyroErrorZ ~ (-0.85)
   yaw =  yaw + GyroZ * elapsedTimeMPU;
   // Serial.println(yaw); // Use to check calibration
   return yaw;
 }
 
 void FindGyroError(){
-  // === Read gyroscope data === //
+  // This function can be called to calculate the gyro error term that can be used above
   while (c < 1000) {
     Wire.beginTransmission(MPU);
     Wire.write(0x43);
     Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 6, true);
-    GyroX = Wire.read() << 8 | Wire.read();
-    GyroY = Wire.read() << 8 | Wire.read();
+    Wire.requestFrom(MPU, 2, true);
     GyroZ = Wire.read() << 8 | Wire.read();
     // Sum all readings
     GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);  // Serial.println(GyroErrorZ);
     c++;
   }
-
   GyroErrorZ = GyroErrorZ / 1000.0;
   // Serial.println(GyroErrorZ);
 }
 
-double computePID(double input,int goalAngle) {
+double computePID(double input,int goalAngle,double P,double I,double D) {
   
-  currentTime = millis();                //get current time
-  elapsedTime = (double)(currentTime - previousTime);  //compute time elapsed from previous computation
+  currentTime = millis();   // find current time
+  elapsedTime = double(currentTime - previousTime);  //compute time elapsed from previous computation
 
-  error = goalAngle - input;                     // determine error
-  cumError += error * elapsedTime;               // compute integral
-  rateError = (error - lastError) / elapsedTime; // compute derivative
+  error = goalAngle - input; // calculate the proportional error
+  cumError += error * elapsedTime; // calculate the integral of the error
+  rateError = (error - lastError) / elapsedTime; // calculate the derivative of the error
 
-  double out = Kp * error + Ki * cumError + Kd * rateError;  //PID output
+  double out = P * error + I * cumError + D * rateError;  //compute the PID output based on the error and the PID constatnts
 
-  lastError = error;                             //remember current error
-  previousTime = currentTime;                    //remember current time
-
-  return out;              //have function return the PID output
-}
-double computePIDturn(double input,int goalAngle) {
-  
-  currentTime = millis();                //get current time
-  elapsedTime = (double)(currentTime - previousTime);  //compute time elapsed from previous computation
-
-  error = goalAngle - input;                     // determine error
-  cumError += error * elapsedTime;               // compute integral
-  rateError = (error - lastError) / elapsedTime; // compute derivative
-
-  double out = Kp_t * error + Ki_t * cumError + Kd_t * rateError;  //PID output
-
-  lastError = error;                             //remember current error
-  previousTime = currentTime;                    //remember current time
-
+  lastError = error;                             //update current error
+  previousTime = currentTime;                    //update current time
   return out;              //have function return the PID output
 }
